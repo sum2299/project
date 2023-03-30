9dcd46adcf90b7e14f632d5dd7c3e1503a177b5b
sum2299 <126958240+sum2299@users.noreply.github.com>
2023-03-30 14:24:09 +0530

    Add files via upload

---
+++ supermarketbiling/src/supermarketbiling/Supermarktbilling.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.supermarketbilling;
+
+/**
+ *
+ * @author HP
+ */
+public class Supermarketbilling {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
